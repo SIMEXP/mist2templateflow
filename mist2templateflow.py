faacf6b7cc6d2aabf923b507bf186ac6c11dc711
Hao-Ting Wang <htwangtw@gmail.com>
2022-02-16 15:09:14 -0500

    add nilearn version

--- mist2templateflow.py
+++ mist2templateflow.py
@@ -18,13 +18,15 @@ from nilearn.image import load_img
 DOWNLOAD_URL = "https://figshare.com/ndownloader/files/9811081"
 TEMPLATE = "MNI152NLin2009bSym"
 ATLAS = "MIST"
+DESCRIPTIONS = [7, 12, 20, 36, 64, 122, 197, 325, 444, "ROI", "ATOM", "Hierarchy"]
+
+INPUT_DIR = "./data/raw"
+OUTPUT_DIR = "./data/processed"
 
 
 def fetch_atlas_mist(dimension, data_dir=None, url=None, resume=True, verbose=1):
-    """Downloads MIST from https://figshare.com/ndownloader/files/9811081
-    """
-    descriptions = [7, 12, 20, 36, 64, 122, 197, 325, 444, "ROI", "Hierarchy", "ATOM"]
-    if dimension not in descriptions:
+    """Downloads MIST from https://figshare.com/ndownloader/files/9811081"""
+    if dimension not in DESCRIPTIONS:
         raise ValueError(f"{dimension} doesn't exist.")
     if url is None:
         url = DOWNLOAD_URL
@@ -65,39 +67,35 @@ def fetch_atlas_mist(dimension, data_dir=None, url=None, resume=True, verbose=1)
 def convert_templateflow(template, atlas, desc):
     folder_name = f"tpl-{template}"
     if desc != "Hierarchy":
-        basenames = f"tpl-{template}_atlas-{atlas}_desc-{desc}_dseg"
+        basenames = f"tpl-{template}_res-03_atlas-{atlas}_desc-{desc}_dseg"
         keys = ["tpf_maps", "tpf_labels"]
         filenames = [os.path.join(folder_name, f'{basenames}.nii.gz'),
                     os.path.join(folder_name, f'{basenames}.tsv')]
     else:
         descs = ["ParcelHierarchyROI", "ParcelHierarchy"]
-        filenames = [os.path.join(folder_name, f"tpl-{template}_atlas-{atlas}_desc-{desc}_dseg.tsv")
+        filenames = [os.path.join(folder_name, f"tpl-{template}_res-03_atlas-{atlas}_desc-{desc}_dseg.tsv")
                      for desc in descs]
         keys = [f"tpf_{desc}" for desc in descs]
     return dict(zip(keys, filenames))
 
 
-descriptions = [7, 12, 20, 36, 64, 122, 197, 325, 444, "ROI", "ATOM", "Hierarchy"]
+if __name__ == "__main__":
+    for desc in DESCRIPTIONS:
+        dataset = fetch_atlas_mist(desc, data_dir=INPUT_DIR)
+        if desc != "Hierarchy":
+            nii = Path(dataset['maps'])
+            output_file = Path(OUTPUT_DIR) / dataset['tpf_maps']
 
-input_dir = "./data/raw"
-output_dir = "./data/processed"
+            if not output_file.parent.is_dir():
+                output_file.parent.mkdir(parents=True, exist_ok=True)
+            shutil.copy(nii, output_file)
 
-for desc in descriptions:
-    dataset = fetch_atlas_mist(desc, data_dir=input_dir)
-    if desc != "Hierarchy":
-        nii = Path(dataset['maps'])
-        output_file = Path(output_dir) / dataset['tpf_maps']
-        shutil.copy(nii, output_file)  # For Python 3.8+.
-        if not output_file.parent.is_dir():
-            output_file.parent.mkdir(parents=True, exist_ok=True)
-        if desc == "ATOM":
-            labels = pd.DataFrame(dataset['labels'], columns=["roi"])
+            if desc == "ATOM":
+                labels = pd.DataFrame(dataset['labels'], columns=["roi"])
+            else:
+                labels = pd.read_csv(dataset['labels'], sep=';')
+            labels.to_csv(os.path.join(OUTPUT_DIR, dataset['tpf_labels']), index=False, sep='\t')
         else:
-            labels = pd.read_csv(dataset['labels'], sep=';')
-        labels.to_csv(os.path.join(output_dir, dataset['tpf_labels']), index=False, sep='\t')
-    else:
-        for label, tpf in zip(["Hierarchy_ROI", "Hierarchy"], ["ParcelHierarchyROI", "ParcelHierarchy"]):
-            df = pd.read_csv(dataset[label])
-            df.to_csv(os.path.join(output_dir, dataset[f"tpf_{tpf}"]), index=False, sep='\t')
-
-
+            for label, tpf in zip(["Hierarchy_ROI", "Hierarchy"], ["ParcelHierarchyROI", "ParcelHierarchy"]):
+                df = pd.read_csv(dataset[label])
+                df.to_csv(os.path.join(OUTPUT_DIR, dataset[f"tpf_{tpf}"]), index=False, sep='\t')
